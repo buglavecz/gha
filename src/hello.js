b8a236206228c71cef81e9f432e41d5b26fa9019
buglavecz <buggio.dev@gmail.com>
2025-12-02 20:08:54 +0100

    dev

--- src/hello.js
+++ src/hello.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+// ESM
+import { consola  } from "consola";
+
+consola.info("Using consola ");
+consola.start("Building project...");
+consola.warn("A new version of consola is available: 3.0.1");
+consola.success("Project built!");
+consola.error(new Error("This is an example error. Everything is fine!"));
+consola.box("I am a simple box");
